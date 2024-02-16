0c416ef80967dd480565a7640901ff00940036d0
merwan <merwan.benzarti@supdevinci-edu.fr>
2024-02-16 10:42:57 +0100

    modification des dossiers

--- carousel-fruit.js
+++ carousel-fruit.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const carouselContainer = document.getElementById('carouselPersonnages');
 
-    // Exemple avec 48 images différentes pour les personnages
+    
     for (let i = 1; i <= 48; i++) {
         const slide = document.createElement('div');
         slide.className = 'slide';
-        slide.style.textAlign = 'center'; // Centre le contenu du slide
+        slide.style.textAlign = 'center'; 
 
         const image = document.createElement('img');
-        // Utilisez un chemin d'accès différent ou un nom différent pour distinguer les images des personnages
+        
         image.src = `image/fruit/${i}.png`; 
         image.alt = `Fruit Image ${i}`;
-        image.style.display = 'block'; // Rend l'image comme un bloc
-        image.style.marginLeft = 'auto'; // Marge gauche automatique pour centrer
-        image.style.marginRight = 'auto'; // Marge droite automatique pour centrer
-        image.style.width = '400px'; // Largeur fixe pour toutes les images
-        image.style.height = '350px'; // Hauteur fixe pour toutes les images
-        image.style.objectFit = 'cover'; // Garde le ratio d'aspect mais coupe l'image si nécessaire
+        image.style.display = 'block'; 
+        image.style.marginLeft = 'auto'; 
+        image.style.marginRight = 'auto'; 
+        image.style.width = '400px'; 
+        image.style.height = '350px'; 
+        image.style.objectFit = 'cover'; 
 
         slide.appendChild(image);
         carouselContainer.appendChild(slide);
